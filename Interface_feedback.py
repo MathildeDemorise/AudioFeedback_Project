27e4312e4c12bfee70b46de15534aff3fe8370d7
Mathilde Demorise <mathilde.demorise@gmail.com>
2023-08-07 12:22:50 +0200

    Updated interface

--- Interface_feedback.py
+++ Interface_feedback.py
@@ -7,6 +7,9 @@ import cv2
 import mediapipe as mp
 import pyttsx3
 import time
+import pygame
+import pandas as pd
+
 
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
@@ -15,8 +18,6 @@ mp_pose = mp.solutions.pose
 ''' 
 This interface is the one I use to try to have a feedback on live
 '''
-
-
 
 ctk.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 ctk.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
@@ -36,15 +37,19 @@ frame_top = ctk.CTkFrame(window, width=int(0.9*screen_width), height=int(0.2*scr
 frame_top.grid(row = 0, column = 0, sticky="nsew", padx=20, pady=20)
 frame_top.grid_propagate(False)
 
-frame_bottom = ctk.CTkFrame(window, width =int(0.9*screen_width), height = int(0.7*screen_height))
-frame_bottom.grid(row=1, column = 0, rowspan = 10, sticky = 'nsew', padx = 20, pady = 20)
+frame_middle = ctk.CTkFrame(window, width =int(0.9*screen_width), height = int(0.6*screen_height))
+frame_middle.grid(row=1, column = 0, sticky = 'nsew', padx = 20, pady = 20)
+frame_middle.grid_propagate(False)
+
+frame_bottom = ctk.CTkFrame(window, width =int(0.9*screen_width), height = int(0.1*screen_height))
+frame_bottom.grid(row=2, column = 0, sticky = 'nsew', padx = 20, pady = 20)
 frame_bottom.grid_propagate(False)
 
-frame_left = ctk.CTkFrame(frame_bottom, width =int(0.45*screen_width), height = int(0.6*screen_height))
+frame_left = ctk.CTkFrame(frame_middle, width =int(0.45*screen_width), height = int(0.6*screen_height))
 frame_left.grid(row=0, column = 0, sticky = 'nsew', padx = 20, pady = 20)
 frame_left.grid_propagate(False)
 
-frame_right = ctk.CTkFrame(frame_bottom, width =int(0.45*screen_width), height = int(0.6*screen_height))
+frame_right = ctk.CTkFrame(frame_middle, width =int(0.45*screen_width), height = int(0.6*screen_height))
 frame_right.grid(row=0, column = 1, sticky = 'nsew', padx = 20, pady = 20)
 frame_right.grid_propagate(False)
 
@@ -53,12 +58,16 @@ frame_right.grid_propagate(False)
 window.columnconfigure(0, weight=1)
 window.rowconfigure(0, weight=1)
 window.rowconfigure(1, weight=1)
+window.rowconfigure(2, weight=1)
 
 frame_top.columnconfigure(0, weight = 1)
 frame_top.columnconfigure(1, weight=1)
 frame_top.rowconfigure(0, weight = 1)
 
+frame_middle.rowconfigure(0, weight = 1)
+
 frame_bottom.rowconfigure(0, weight = 1)
+frame_bottom.columnconfigure(0, weight = 1)
 
 frame_left.columnconfigure(0, weight=1)
 frame_left.rowconfigure(0, weight=1)
@@ -70,6 +79,20 @@ frame_right.rowconfigure(0, weight=1)
 
 path_exercise = ""
 name_exercise = ""
+list_of_data_angle = []
+state = 'on'
+
+# Usefull files and data
+
+instructions = pd.read_excel("Instructions.xlsx", header=0, sheet_name='Exercices')
+sheet_angle = pd.read_excel("Instructions.xlsx", header=0, sheet_name='Angle')
+
+row_1, _ = instructions.shape
+row_2, _ = sheet_angle.shape
+
+for i in range(0, row_2):
+    list_of_data_angle.append(sheet_angle.iloc[i,0])
+    
 
 # Create functions
 
@@ -86,6 +109,11 @@ def select_exercise():
     select_exercise_button.configure(text = name_exercise)
 
 def start_training():
+
+    for i in range (0, row_1):
+        if name_exercise == instructions.iloc[i,0] :
+            corresponding_angle = instructions.iloc[0,1]
+            corresponding_commentary = instructions.iloc[0,2]
     
     video = cv2.VideoCapture(path_exercise)
 
@@ -93,7 +121,7 @@ def start_training():
 
         if not hasattr(show_video, 'canvas1_created'):
             
-            show_video.canvas1 = ctk.CTkCanvas(frame_left,  width =int(0.45*screen_width), height = int(0.6*screen_height))
+            show_video.canvas1 = ctk.CTkCanvas(frame_left,  width =int(0.45*screen_width), height = int(0.5*screen_height))
             show_video.canvas1.grid(row=0, column=0, padx=(10, 10), pady=(10, 10), sticky="nsew")
             show_video.canvas1_created = True
     
@@ -113,34 +141,21 @@ def start_training():
                 
             window.update()
                 
-            if cv2.waitKey(10) & 0xFF == ord('q'): 
+            if state == 'off': 
                 break
 
         cap.release()
         cv2.destroyAllWindows()
 
-    show_video(video, int(0.67*screen_width), int(0.91*screen_height))
-
-    time.sleep(1)
-
-    def text_to_speech(text1):
+    # show_video(video, int(0.67*screen_width), int(0.81*screen_height))
+    
+    def text_to_speech(text):
         engine = pyttsx3.init()
-        engine.say(text1)
+        engine.setProperty('voice', "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Speech\\Voices\\Tokens\\TTS_MS_EN-US_ZIRA_11.0")
+        engine.say(text)
         engine.runAndWait()
 
-    text_to_speech("NOW IT'S YOUR TURN")
-
-    # def countdown(seconds):
-    #     if seconds > 0:
-    #         label.configure(text=str(seconds))
-    #         window.after(1000, countdown, seconds - 1)
-    #     else:
-    #         label.configure(text="")
-    #         return 
-
-    # countdown(5)
-
-    time.sleep(5)
+    text_to_speech(corresponding_commentary)
 
     def calculate_angle(a,b,c):
         a = np.array(a) # First
@@ -159,7 +174,7 @@ def start_training():
 
         if not hasattr(show_angle_on_live, 'canvas2_created'):
             
-            show_angle_on_live.canvas2 = ctk.CTkCanvas(frame_right, width =int(0.45*screen_width), height = int(0.6*screen_height))
+            show_angle_on_live.canvas2 = ctk.CTkCanvas(frame_right, width =int(0.45*screen_width), height = int(0.5*screen_height))
             show_angle_on_live.canvas2.grid(row=0, column=0, padx=(10, 10), pady=(10, 10), sticky="nsew")
             show_angle_on_live.canvas2_created = True
 
@@ -169,7 +184,7 @@ def start_training():
                 _, frame = cap.read()
 
                 flipped_frame = cv2.flip(frame, 1)
-                resized_frame = cv2.resize(flipped_frame, (int(0.67*screen_width), int(0.91*screen_height)))
+                resized_frame = cv2.resize(flipped_frame, (int(0.67*screen_width), int(0.81*screen_height)))
                 
                 # Recolor image to RGB
                 
@@ -236,11 +251,41 @@ def start_training():
 
                 window.update()
 
+                if state=='off':
+                    break
+
+
             cap.release()
             cv2.destroyAllWindows()
 
-    show_angle_on_live(cv2.VideoCapture(0), 15, 13, 11)
+    def get_points_of_interest(file):
 
+        selected_joint1 = []
+        selected_joint2 = []
+        selected_joint3 = []
+                        
+        for i,angle in enumerate(list_of_data_angle):
+            
+            if corresponding_angle==angle:
+                selected_joint1=file.iloc[i,1]
+                selected_joint2=file.iloc[i,2]
+                selected_joint3=file.iloc[i,3]
+
+        return selected_joint1, selected_joint2, selected_joint3
+                
+    angle1, angle2, angle3 = get_points_of_interest(sheet_angle)
+
+    show_angle_on_live(cv2.VideoCapture(0), angle1, angle2, angle3)
+
+def close_webcam_or_video():
+    global state
+    if state == 'on' :
+        state = 'off'
+        return state
+    if state == 'off' :
+        state = 'on'
+        return state
+    
 # Create button
 
 select_exercise_button = ctk.CTkButton(frame_top, text="Select the exercise's video", command=select_exercise, width= 300, height = 50, anchor='center', font = ("Helvetica", 16, "bold"))
@@ -248,6 +293,9 @@ select_exercise_button.grid(row=0, column=0,  padx=20, pady=(10, 10))
 
 start_exercice = ctk.CTkButton(frame_top, text="Start the exercise", command=start_training, width= 300, height = 50, anchor='center', font = ("Helvetica", 16, "bold"))
 start_exercice.grid(row=0, column=1,  padx=20, pady=(10, 10))
+
+close_webcam_button = ctk.CTkButton(frame_bottom, text="Stop the session", command=close_webcam_or_video)
+close_webcam_button.grid(row=3, column=0, padx=20, pady=(10, 10))
 
 label = ctk.CTkLabel(frame_right, text="")
 label.grid(row=0, column=0, padx=20, pady=20)
